b1957ae5c462b4165d17bc2ac648723c4373acbd
Mischa Jörg <mischa@ninetyfour.ch>
2022-09-25 15:31:16 +0200

    chore: align menu button style

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -124,7 +124,7 @@ export default function Navigation({ children }: any) {
                   <div className="space-y-6 py-6 px-5">
                     <a
                       href="#"
-                      className="flex w-full items-center justify-center rounded-md px-4 py-2 text-base  text-white shadow-sm gradient-background"
+                      className="ml-3 xl:ml-6 inline-flex items-center justify-center whitespace-nowrap rounded-md border-2 border-white px-4 py-2 text-base text-white hover:bg-white hover:text-blue-500"
                     >
                       Anmelden
                     </a>
