d35c0104a7df6f6dcda03aec4331a1c3e50b97af
Mischa Jörg <mischa@ninetyfour.ch>
2023-02-24 18:10:40 +0100

    feat: add horse parcour information

--- pages/informationen.tsx
+++ pages/informationen.tsx
@@ -7,103 +7,114 @@ import Layout from '../components/layout';
 import Timetable from '../components/timetable';
 
 const quicklinks = [
-  { name: 'Ablauf Wettkampftag', stat: 'Zeitplan', link: '#zeitplan', target: '' },
-  { name: 'Badi Hüttwilersee', stat: 'Wettkampfzentrum', link: '#wettkampfzentrum', target: '' },
-  { name: 'Oft gestellte Frage', stat: 'FAQ', link: '#faq', target: '' },
-  { name: 'Wettkampfbestimmungen', stat: 'Reglement', link: '/dokumente/2020_reglement.pdf', target: '_blank' },
-  { name: 'Fotos & Videos', stat: 'Impressionen', link: 'https://next.tiny-rocket.ch/s/AXgbWcyae2EpXs9', target: '_blank' },
-  { name: 'Team vervollständigen', stat: 'Teambörse', link: '/teamboerse', target: '' },
+    { name: 'Ablauf Wettkampftag', stat: 'Zeitplan', link: '#zeitplan', target: '' },
+    { name: 'Badi Hüttwilersee', stat: 'Wettkampfzentrum', link: '#wettkampfzentrum', target: '' },
+    { name: 'Oft gestellte Frage', stat: 'FAQ', link: '#faq', target: '' },
+    { name: 'Wettkampfbestimmungen', stat: 'Reglement', link: '/dokumente/2020_reglement.pdf', target: '_blank' },
+    { name: 'Fotos & Videos', stat: 'Impressionen', link: 'https://next.tiny-rocket.ch/s/AXgbWcyae2EpXs9', target: '_blank' },
+    { name: 'Hindernisse und Strecke', stat: 'Reitparcours', link: '#reitparcours', target: '' },
+    { name: 'Team vervollständigen', stat: 'Teambörse', link: '/teamboerse', target: '' },
 ];
 
 const Informationen: NextPage = () => {
-  return (
-    <Layout>
-      <div className="content">
-        <h1 className="h1">Informationen</h1>
-        <h2 className="h2">
-          Hier findest Du die wichtigsten Eckdaten und allgemeine Infos zum Wettkampftag; vom Wettkampfzentrum über den Zeitplan bis hin zur Verpflegung.
-        </h2>
-      </div>
-
-      <div className="content-wide">
-        <div className="relative w-full h-60 sm:h-80 md:h-[30rem]">
-          <Image
-            objectFit="cover"
-            objectPosition={"center"}
-            layout={'fill'}
-            className="image"
-            src={'/fotos/slider/35.jpg'}
-            alt="People working on laptops"
-          />
-        </div>
-      </div>
-
-      <div className="content">
-        <h3 className="h4">Übersicht</h3>
-
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
-          {quicklinks.map((item) => (
-            <Link href={item.link}>
-              <a target={item.target}>
-                <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg group">
-                  <ArrowUpRightIcon
-                    className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
-                    aria-hidden="true"
-                  />
-
-                  <div className="grid grid-cols-1">
-                    <div className="col-span-1 text-base text-gray-900">{item.name}</div>
-
-                    <div className="flex justify-between items-baseline">
-                      <div className="flex items-baseline text-2xl font-semibold text-blue-600">
-                        {item.stat}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </Link>
-          ))}
-        </div>
-      </div>
-
-      <div className="content">
-        <h3 className="h4" id="zeitplan">Zeitplan</h3>
-        <Timetable />
-      </div>
-
-      <div className="content">
-        <h3 className="h4" id="wettkampfzentrum">Wettkampfzentrum</h3>
-        <p className="p">
-          Das Wettkampfzentrum liegt beim <a href="https://goo.gl/maps/PmSss7vXpTofKFjy8" className="link">Strandbad Hüttwilen</a>.
-          Dort befindet sich nebst Start- und Ziellinie auch die gesamte Infrastruktur für die Athleten und Zuschauer.
-          Die Details kannst Du dem Plan unterhalb entnehmen.
-        </p>
-        <p className="p">
-          Der Parkplatz sowie der Reitparcour können je nach Fruchtfolge an einem leicht anderen Ort sein - sie sind aber entsprechend beschriftet ab der Einfahrt Badi Hüttwilen.
-        </p>
-        <Link href="/dokumente/2020_wechselzone.png">
-          <a target="_blank" className="group relative inline-block image">
-            <div className='absolute text-black flex justify-center items-center top-0 left-0 right-0 bottom-0 invisible group-hover:visible z-10  group-hover:backdrop-blur-sm goup-hover:bg-white/30 image'>
-              <p className="">In neuem Fenster öffnen</p>
-              <ArrowUpRightIcon
-                className="ml-2 h-5 w-5"
-                aria-hidden="true"
-              />
-
+    return (
+        <Layout>
+            <div className="content">
+                <h1 className="h1">Informationen</h1>
+                <h2 className="h2">
+                    Hier findest Du die wichtigsten Eckdaten und allgemeine Infos zum Wettkampftag; vom Wettkampfzentrum über den Zeitplan bis hin zur Verpflegung.
+                </h2>
             </div>
 
-            <img className='image h-96 z-0' src="/dokumente/2020_wechselzone.png"></img>
-          </a>
-        </Link>
-      </div>
+            <div className="content-wide">
+                <div className="relative w-full h-60 sm:h-80 md:h-[30rem]">
+                    <Image
+                        objectFit="cover"
+                        objectPosition={"center"}
+                        layout={'fill'}
+                        className="image"
+                        src={'/fotos/slider/35.jpg'}
+                        alt="People working on laptops"
+                    />
+                </div>
+            </div>
 
-      <div className="content" id="faq">
-        <FAQ />
-      </div>
+            <div className="content">
+                <h3 className="h4">Übersicht</h3>
 
-    </Layout>
-  )
+                <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
+                    {quicklinks.map((item) => (
+                        <Link href={item.link}>
+                            <a target={item.target}>
+                                <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg group">
+                                    <ArrowUpRightIcon
+                                        className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
+                                        aria-hidden="true"
+                                    />
+
+                                    <div className="grid grid-cols-1">
+                                        <div className="col-span-1 text-base text-gray-900">{item.name}</div>
+
+                                        <div className="flex justify-between items-baseline">
+                                            <div className="flex items-baseline text-2xl font-semibold text-blue-600">
+                                                {item.stat}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+            <div className="content">
+                <h3 className="h4" id="zeitplan">Zeitplan</h3>
+                <Timetable />
+            </div>
+
+            <div className="content">
+                <h3 className="h4" id="wettkampfzentrum">Wettkampfzentrum</h3>
+                <p className="p">
+                    Das Wettkampfzentrum liegt beim <a href="https://goo.gl/maps/PmSss7vXpTofKFjy8" className="link">Strandbad Hüttwilen</a>.
+                    Dort befindet sich nebst Start- und Ziellinie auch die gesamte Infrastruktur für die Athleten und Zuschauer.
+                    Die Details kannst Du dem Plan unterhalb entnehmen.
+                </p>
+                <p className="p">
+                    Der Parkplatz sowie der Reitparcour können je nach Fruchtfolge an einem leicht anderen Ort sein - sie sind aber entsprechend beschriftet ab der Einfahrt Badi Hüttwilen.
+                </p>
+                <Link href="/dokumente/2020_wechselzone.png">
+                    <a target="_blank" className="group relative inline-block image">
+                        <div className='absolute text-black flex justify-center items-center top-0 left-0 right-0 bottom-0 invisible group-hover:visible z-10  group-hover:backdrop-blur-sm goup-hover:bg-white/30 image'>
+                            <p className="">In neuem Fenster öffnen</p>
+                            <ArrowUpRightIcon
+                                className="ml-2 h-5 w-5"
+                                aria-hidden="true"
+                            />
+
+                        </div>
+
+                        <img className='image h-96 z-0' src="/dokumente/2020_wechselzone.png"></img>
+                    </a>
+                </Link>
+            </div>
+
+            <div className="content">
+                <h3 className="h4" id="reitparcours">Reitparcours</h3>
+                <p className="p">
+                    Der Reiterparcours wird aus verschiedenen Gymkhana Hindernissen zusammensetzen. Der Parcours wird hier spätestens 4 Wochen vorher aufgeschaltet. Sprünge, nicht höher als 30cm, können vorhanden sein.
+                </p>
+                <p className="p">
+                    Ob Pony oder Pferd, ob Englisch- oder Westernreiten, es sind alle Willkommen und der Parcours kann von allen gemeistert werden. Wer noch kein Team hat, kann sich gerne bei der <a href={'/teamboerse'} className='underline underline-offset-2'>Teambörse</a> registrieren - es melden sich oft Teams an, welche noch Reiter:innen suchen.
+                </p>
+            </div>
+
+            <div className="content" id="faq">
+                <FAQ />
+            </div>
+
+        </Layout>
+    )
 }
 
 export default Informationen;
--- pages/stafette.tsx
+++ pages/stafette.tsx
@@ -6,174 +6,179 @@ import Infos, { InfoPoint, InfoPointCycling, InfoPointRun, InfoPointSwim } from 
 import Layout from '../components/layout';
 
 const statsShortDistance = [
-  { name: 'Schwimmen', stat: '0.5 km', change: '0 m', link: 'https://connect.garmin.com/modern/course/29506834' },
-  { name: 'Rad', stat: '20.64 km', change: '260 m', link: 'https://connect.garmin.com/modern/course/29504861' },
-  { name: 'Bike', stat: '6.6 km', change: '150 m', link: 'https://connect.garmin.com/modern/course/29509069' },
-  { name: 'Lauf', stat: '6.2 km', change: '52 m', link: 'https://connect.garmin.com/modern/course/29507069' },
+    { name: 'Schwimmen', stat: '0.5 km', change: '0 m', link: 'https://connect.garmin.com/modern/course/29506834' },
+    { name: 'Rad', stat: '20.64 km', change: '260 m', link: 'https://connect.garmin.com/modern/course/29504861' },
+    { name: 'Bike', stat: '6.6 km', change: '150 m', link: 'https://connect.garmin.com/modern/course/29509069' },
+    { name: 'Lauf', stat: '6.2 km', change: '52 m', link: 'https://connect.garmin.com/modern/course/29507069' },
 ]
 const statsOlympicDistance = [
-  { name: 'Schwimmen', stat: '1 km', change: '0 m', link: 'https://connect.garmin.com/modern/course/29506971' },
-  { name: 'Rad', stat: '38.2 km', change: '500 m', link: 'https://connect.garmin.com/modern/course/29506670' },
-  { name: 'Bike', stat: '12.6 km', change: '220 m', link: 'https://connect.garmin.com/modern/course/29529369' },
-  { name: 'Lauf', stat: '10.55 km', change: '100 m', link: 'https://connect.garmin.com/modern/course/29507170' },
+    { name: 'Schwimmen', stat: '1 km', change: '0 m', link: 'https://connect.garmin.com/modern/course/29506971' },
+    { name: 'Rad', stat: '38.2 km', change: '500 m', link: 'https://connect.garmin.com/modern/course/29506670' },
+    { name: 'Bike', stat: '12.6 km', change: '220 m', link: 'https://connect.garmin.com/modern/course/29529369' },
+    { name: 'Lauf', stat: '10.55 km', change: '100 m', link: 'https://connect.garmin.com/modern/course/29507170' },
 ]
 
 {/* TODOs: gleiche Infos wie bei Triathlon */ }
 {/* Reitparcours -> link zum doc */ }
 
 const infos: InfoPoint[] = [
-  {
-    name: 'Start',
-    icon: (props: any) => (<RocketLaunchIcon {...props} />),
-    description: 'Bei den Stafette-Kategorien gibt es jeweils einen Massenstart am Hüttwilersee.'
-  },
-  InfoPointSwim,
-  InfoPointCycling,
-  {
-    name: 'Biken',
-    icon: (props: any) => (<MapIcon {...props} />),
-    description: 'Die Bikestrecke beinhaltet Sprünge, deshalb sind Mountainbikes empfohlen. Gravelbikes können auf eigene Verwantwortung benutzt werden.'
-  },
-  InfoPointRun,
-  {
-    name: 'Teambörse',
-    icon: (props: any) => (<UserGroupIcon {...props} />),
-    description: 'Die Stafette ist ein Team-Wettkampf und es werden nur vollständige Teams zugelassen. Wir haben aber weiter oben eine Teambörse verlinkt, bei der du entweder einen Platz im Team oder noch fehlende Mitglieder suchen kannst.'
-  },
+    {
+        name: 'Start',
+        icon: (props: any) => (<RocketLaunchIcon {...props} />),
+        description: 'Bei den Stafette-Kategorien gibt es jeweils einen Massenstart am Hüttwilersee.'
+    },
+    InfoPointSwim,
+    InfoPointCycling,
+    {
+        name: 'Biken',
+        icon: (props: any) => (<MapIcon {...props} />),
+        description: 'Die Bikestrecke beinhaltet Sprünge, deshalb sind Mountainbikes empfohlen. Gravelbikes können auf eigene Verwantwortung benutzt werden.'
+    },
+    InfoPointRun,
+    {
+        name: 'Reiten',
+        icon: (props: any) => (<MapIcon {...props} />),
+        description: 'Der Reiterparcours wird aus verschiedenen Gymkhana Hindernissen zusammengesetzt sein. Sprünge, nicht höher als 30cm, können vorhanden sein.'
+    },
+    {
+        name: 'Teambörse',
+        icon: (props: any) => (<UserGroupIcon {...props} />),
+        description: 'Die Stafette ist ein Team-Wettkampf und es werden nur vollständige Teams zugelassen. Wir haben aber weiter oben eine Teambörse verlinkt, bei der du entweder einen Platz im Team oder noch fehlende Mitglieder suchen kannst.'
+    },
 ];
 
 const Stafette: NextPage = () => {
-  return (
-    <Layout>
-      <div className="content">
-        <h1 className="h1">Stafette</h1>
-        <h2 className="h2 width">
-          Die älteste Kategorie der DSS - und die coolste!
-          Im Gegensatz zum Triathlon kommen hier auch die Biker zum Zug!
-        </h2>
-      </div>
-
-      <div className="content-wide">
-        <div className="relative w-full h-60 sm:h-80 md:h-[30rem]">
-          <Image
-            objectFit="cover"
-            objectPosition={"center"}
-            layout={'fill'}
-            className="image"
-            src={'/event/ziel.jpg'}
-            alt="People working on laptops"
-          />
-        </div>
-      </div>
-
-      <div className="content">
-        <h3 className="h4">Allgemeine Infos</h3>
-        <p className="p">
-          Alle Kategorien starten im Wettkampfzentrum am Hüttwilersee.
-          Eine Karte vom Wettkampfzentrum und allgemeine Athleteninfos findest du unter <Link href="/informationen"><span className="link" >Informationen</span></Link>. <span className="p italic">
-            Neu bieten wir dieses Jahr auch eine Teambörse an, damit du dein Team ergänzen oder als einzelner Athlet nach einem Team suchen kannst: <Link href="/teamboerse"><span className="link" >zur Teambörse</span></Link>.
-          </span>
-        </p>
-
-        <Infos props={infos} />
-      </div>
-
-      <div className="content">
-        <h3 className="h4">Strecken Stafette Kurz</h3>
-        <p className="p">
-          Für die Kategorie Stafette Kurz gibt es zwei Wertungen: mit bzw. ohne Reiter.
-          Der Parcour variiert von Jahr zu Jahr aufgrund der Fruchtfolge der umliegenden Kulturzonen.
-        </p>
-
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
-          {statsShortDistance.map((item) => (
-            <a href={item.link} target="_blank">
-              <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
-                <ArrowUpRightIcon
-                  className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
-                  aria-hidden="true"
-                />
-
-                <div className="grid grid-cols-1">
-                  <div className="col-span-1 text-base text-gray-900">{item.name}</div>
-
-                  <div className="flex justify-between items-baseline">
-                    <div className="flex items-baseline text-2xl font-semibold text-blue-600">
-                      {item.stat}
-                    </div>
-                    <div
-                      className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
-                      <ArrowTrendingUpIcon
-                        className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
-                        aria-hidden="true"
-                      />
-                      {item.change}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          ))}
-          <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
-            <div className="grid grid-cols-1">
-              <div className="col-span-1 text-base text-gray-900">Reiten (optional)</div>
-
-              <div className="flex justify-between items-baseline">
-                <div className="flex items-baseline text-2xl font-semibold text-blue-600">
-                  ~ 1 km
-                </div>
-                <div
-                  className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
-                  <ArrowTrendingUpIcon
-                    className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
-                    aria-hidden="true"
-                  />
-                  0 m
-                </div>
-              </div>
+    return (
+        <Layout>
+            <div className="content">
+                <h1 className="h1">Stafette</h1>
+                <h2 className="h2 width">
+                    Die älteste Kategorie der DSS - und die coolste!
+                    Im Gegensatz zum Triathlon kommen hier auch die Biker zum Zug!
+                </h2>
             </div>
-          </div>
 
-        </div>
-      </div>
-
-      <div className="content">
-        <h3 className="h4">Strecken Stafette Lang</h3>
-
-        <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
-          {statsOlympicDistance.map((item) => (
-            <a href={item.link} target="_blank">
-              <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
-                <ArrowUpRightIcon
-                  className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
-                  aria-hidden="true"
-                />
-
-                <div className="grid grid-cols-1">
-                  <div className="col-span-1 text-base text-gray-900">{item.name}</div>
-
-                  <div className="flex justify-between items-baseline">
-                    <div className="flex items-baseline text-2xl font-semibold text-blue-600">
-                      {item.stat}
-                    </div>
-                    <div
-                      className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
-                      <ArrowTrendingUpIcon
-                        className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
-                        aria-hidden="true"
-                      />
-                      {item.change}
-                    </div>
-                  </div>
+            <div className="content-wide">
+                <div className="relative w-full h-60 sm:h-80 md:h-[30rem]">
+                    <Image
+                        objectFit="cover"
+                        objectPosition={"center"}
+                        layout={'fill'}
+                        className="image"
+                        src={'/event/ziel.jpg'}
+                        alt="People working on laptops"
+                    />
                 </div>
-              </div>
-            </a>
-          ))}
-        </div>
-      </div>
+            </div>
 
-    </Layout>
-  )
+            <div className="content">
+                <h3 className="h4">Allgemeine Infos</h3>
+                <p className="p">
+                    Alle Kategorien starten im Wettkampfzentrum am Hüttwilersee.
+                    Eine Karte vom Wettkampfzentrum und allgemeine Athleteninfos findest du unter <Link href="/informationen"><span className="link" >Informationen</span></Link>. <span className="p italic">
+                        Neu bieten wir dieses Jahr auch eine Teambörse an, damit du dein Team ergänzen oder als einzelner Athlet nach einem Team suchen kannst: <Link href="/teamboerse"><span className="link" >zur Teambörse</span></Link>.
+                    </span>
+                </p>
+
+                <Infos props={infos} />
+            </div>
+
+            <div className="content">
+                <h3 className="h4">Strecken Stafette Kurz</h3>
+                <p className="p">
+                    Für die Kategorie Stafette Kurz gibt es zwei Wertungen: mit bzw. ohne Reiter.
+                    Der Parcour variiert von Jahr zu Jahr aufgrund der Fruchtfolge der umliegenden Kulturzonen.
+                </p>
+
+                <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
+                    {statsShortDistance.map((item) => (
+                        <a href={item.link} target="_blank">
+                            <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
+                                <ArrowUpRightIcon
+                                    className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
+                                    aria-hidden="true"
+                                />
+
+                                <div className="grid grid-cols-1">
+                                    <div className="col-span-1 text-base text-gray-900">{item.name}</div>
+
+                                    <div className="flex justify-between items-baseline">
+                                        <div className="flex items-baseline text-2xl font-semibold text-blue-600">
+                                            {item.stat}
+                                        </div>
+                                        <div
+                                            className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
+                                            <ArrowTrendingUpIcon
+                                                className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
+                                                aria-hidden="true"
+                                            />
+                                            {item.change}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    ))}
+                    <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
+                        <div className="grid grid-cols-1">
+                            <div className="col-span-1 text-base text-gray-900">Reiten (optional)</div>
+
+                            <div className="flex justify-between items-baseline">
+                                <div className="flex items-baseline text-2xl font-semibold text-blue-600">
+                                    ~ 1 km
+                                </div>
+                                <div
+                                    className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
+                                    <ArrowTrendingUpIcon
+                                        className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
+                                        aria-hidden="true"
+                                    />
+                                    0 m
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="content">
+                <h3 className="h4">Strecken Stafette Lang</h3>
+
+                <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:mt-8">
+                    {statsOlympicDistance.map((item) => (
+                        <a href={item.link} target="_blank">
+                            <div className="relative col-span-1 bg-gray-50 hover:bg-gray-100 py-8 px-8 rounded-lg border-2 border-transparent group">
+                                <ArrowUpRightIcon
+                                    className="h-5 w-5 absolute top-2 right-2 group-hover:visible text-gray-300 group-hover:text-blue-500"
+                                    aria-hidden="true"
+                                />
+
+                                <div className="grid grid-cols-1">
+                                    <div className="col-span-1 text-base text-gray-900">{item.name}</div>
+
+                                    <div className="flex justify-between items-baseline">
+                                        <div className="flex items-baseline text-2xl font-semibold text-blue-600">
+                                            {item.stat}
+                                        </div>
+                                        <div
+                                            className="inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0 bg-blue-100">
+                                            <ArrowTrendingUpIcon
+                                                className="-ml-1 mr-2 h-5 w-5 flex-shrink-0 self-center text-blue-600"
+                                                aria-hidden="true"
+                                            />
+                                            {item.change}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    ))}
+                </div>
+            </div>
+
+        </Layout>
+    )
 }
 
 export default Stafette;
