36fc576187b2bc15db344160719ec1202602ad0a
Mischa Jörg <mischa@ninetyfour.ch>
2024-02-27 20:48:20 +0100

    git: the great reset

--- components/tv.tsx
+++
@@ -1,9 +0,0 @@
-export default function TV() {
-    return (
-        <div className='rounded-3xl bg-gray-50 p-8'>
-            <img className="md:w-1/2 md:mx-auto mb-8 md:mb-12" src={'/logo/dss_tv_3.svg'} alt="Dreiseenstafette Logo" />
-            <p className="text-center">Die Dreiseenstafette ist seit jeher vom TV Hüttwilen organisiert.</p>
-        </div>
-
-    )
-}
--- pages/api/helfer.ts
+++
@@ -1,9 +0,0 @@
-export default function handler(req: any, res: any) {
-  const body = req.body
-
-  res.status(200).json({
-    message: 'Hi there! This is not yet operational. Maybe you can check again later.',
-    email: `${body.email}`,
-    password: `${body.password}`,
-  })
-}
