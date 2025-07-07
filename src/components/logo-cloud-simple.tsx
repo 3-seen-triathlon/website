44216922a1d7be965b66bd1578d758abd05ca47e
Mischa Jörg <mischa@ninetyfour.ch>
2025-07-07 22:43:25 +0200

    landing page: update partners
    Signed-off-by: Mischa Jörg <mischa@ninetyfour.ch>

--- src/components/logo-cloud-simple.tsx
+++ src/components/logo-cloud-simple.tsx
@@ -1,86 +1,6 @@
 import { Container } from "./container";
 import { Heading, Lead, Subheading } from "./text";
-
-const sponsoren = [
-  {
-    name: "Raiffeisenbank Seerücken",
-    logos: [
-      "/logo/raiffeisen.png"
-    ],
-    category: "Hauptsponsor",
-  },
-  {
-    name: "Die Mobiliar Generalagentur Frauenfeld",
-    logos: [
-      "/logo/mobiliar.png"
-    ],
-    category: "Hauptsponsor",
-  },
-  {
-    name: "Nüssli",
-    logos: [
-      "/2025/partner/nussli.png",
-    ],
-    category: "Olypmic Distance",
-  },
-  {
-    name: "Ruba",
-    logos: [
-      "/2025/partner/ruba_nussli.png"
-    ],
-    category: "Olypmic Distance",
-  },
-
-  {
-    name: "ThurPlus",
-    logos: [
-      "/2025/partner/thurplus.png"
-    ],
-    category: "Badekappen",
-  },
-  {
-    name: "EWE Elektro AG",
-    logos: [
-      "/2025/partner/ewe.png",
-    ],
-    category: "Supplier"
-  },
-  {
-    name: "Hans Hagen AG",
-    logos: [
-      "/2025/partner/hans_hagen.png",
-    ],
-    category: "Supplier"
-  },
-  {
-    name: "Stieger Gartenbau",
-    logos: [
-      "/2025/partner/stieger.png",
-    ],
-    category: "Supplier"
-  },
-  {
-    name: "Landi Seebachtal",
-    logos: [
-      "/2025/partner/landi.png",
-    ],
-    category: "Supplier"
-  },
-  {
-    name: "Fahnencenter Weinfelden",
-    logos: [
-      "/2025/partner/fahnencenter.png",
-    ],
-    category: "Supplier"
-  },
-  {
-    name: "Gebrüder Umbricht",
-    logos: [
-      "/2025/partner/umbricht.png",
-    ],
-    category: "Supplier"
-  },
-]
+import sponsors from '../data/data.json'
 
 export default function LogoCloudSimple() {
   return (
@@ -96,21 +16,32 @@ export default function LogoCloudSimple() {
           </Lead>
         </div>
 
-        <div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-2">
-          {sponsoren.map(sponsor => (
-            <div key={sponsor.name} className="bg-gray-400/5 p-4 sm:p-8 grid grid-cols-1 gap-y-10 align-middle text-center">
-              <div key={sponsor.name} className="flex justify-center flex-wrap items-center gap-y-2 sm:gap-y-4">
-                {sponsor.logos.map(logo => (
-                  <img
-                    key={logo}
-                    alt={sponsor.name}
-                    src={logo}
-                    width={200}
-                    height={50}
-                    className={`${sponsor.logos.length == 1 ? "max-h-12 md:max-h-20" : "max-h-8 md:max-h-10"} w-full object-contain`}
-                  />
-                ))}
-              </div>
+        <div className="grid-cols-2 grid-cols-3 grid-cols-4" />
+
+        <div className="space-y-8">
+          {sponsors.categories.map(category => (
+            <div key={category.name}>
+              {category.partner.length > 0 && (
+                <div>
+                  <Lead>{category.name}</Lead>
+                  <div className={`mt-6 -mx-6 grid grid-cols-${category.grid} gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-${category.grid}`}>
+                    {category.partner.map(sponsor => (
+                      <a href={sponsor.link} target="_blank" key={sponsor.name} className="bg-gray-400/5 p-4 sm:p-8 grid grid-cols-1 gap-y-10 align-middle text-center">
+                        <div key={sponsor.name} className="flex justify-center flex-wrap items-center gap-y-2 sm:gap-y-4">
+                          <img
+                            key={sponsor.logo}
+                            alt={sponsor.name}
+                            src={sponsor.logo}
+                            width={200}
+                            height={50}
+                            className="max-h-12 md:max-h-20 w-full object-contain"
+                          />
+                        </div>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
           ))}
         </div>
